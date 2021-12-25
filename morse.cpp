8359254b5946688091d682c0a8e086e78b814df3
PhongnsEE2 <phong2537vn@gmail.com>
2021-12-25 19:32:14 +0700

    checkFileMorse

--- morse.cpp
+++ morse.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstring>
 
-#define MAX 255
+#define MAX 1000
 
 using namespace std;
 
@@ -13,13 +14,26 @@ bool FileExist1(const string &filename)
     return f.good();//Check whether the status of the stream is normal. Return true when the error status flags (eofbit, failbit and badbit) are not set
 }
 
+bool checkIfMorse(const string& textInFile)
+{
+    int i;
+    for (i=0; i < textInFile.length(); i++)
+    {
+        char temp = char(textInFile[i]);
+        if ((temp != '.') || (temp != '/') || (temp != ' ') || (temp != '-') || (temp != '\n'))
+            return false;
+    }
+    return true;
+}
+
 int main(int argc, char *argv[])
 {
-    char temp[MAX];
+    char temp[1];
     string mystring;
     ofstream filera;
     ifstream filevao;
 
+    // Open filevao and get string into mystring
     try
     {
         // Error: FILENAME could not be opened
@@ -32,8 +46,12 @@ int main(int argc, char *argv[])
         cerr<< "Error: "<< argv[1]<< " "<< err<< endl;
         exit(1);
     }
+    mystring = string((istreambuf_iterator<char>(filevao)), istreambuf_iterator<char>());
+    if (checkIfMorse(mystring))
+        cout << "chuan";
+    filevao.close();
 
-    // Warning: FILENAME already ...
+    // Warning: FILENAME already if no => exit
     if (FileExist1(argv[2])) 
     {
         cout << "Warning: " << argv[2] << " already exists. Do you wish to overwrite (y,n)? ";
@@ -42,16 +60,9 @@ int main(int argc, char *argv[])
         if (yesNo != 'y')
             exit(1); 
     }
-
     filera.open(argv[2], ios::out | ios::trunc);
-
-    while (!filevao.eof())
-    {
-        filevao.getline(temp, MAX);
-        mystring = string(temp);
-    }
-
-    filevao.close();
+    
+    
 
     string ra[64]={
     "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",
@@ -80,8 +91,8 @@ int main(int argc, char *argv[])
     string phan_tach = " ";
     int pos = 0;
     string token;
-
     int line = 1;
+
     while ((pos = mystring.find(phan_tach)) != string::npos) 
     {
         token = mystring.substr(0, pos);
@@ -94,15 +105,22 @@ int main(int argc, char *argv[])
                 filera << token;
                 break;
             }
-            else if (i==64)
+            else if ((i==64) && (token.c_str()[0] == '\n'))
+            {
+                token = "\n";
+                line++;
+                filera << token;
+            }
+            else if ((i==64) && (token.c_str()[0] != '\n')) 
             {
                 filera << "*";
-                cerr << "Error: Invalid Morse code "<< token << "on line "<< line;
-                if (token.c_str()[0] == '\n')
-                    line++;
+                cerr << "Error: Invalid Morse code "<< token << " on line "<< line << endl;        
             }
         }
-        mystring.erase(0, pos + phan_tach.length());
+        if (token != "\n")
+            mystring.erase(0, pos + phan_tach.length());
+        else
+            mystring.erase(0, pos);
     }
     filera.close();
 
